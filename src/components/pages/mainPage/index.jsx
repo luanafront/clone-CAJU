01990806a925d80c8f59cbd93c3dbb4c32facdab
luanafront <luana.lopes.rodrigues1@gmail.com>
2022-06-14 20:25:24 -0300

    cofigure and set color in navigator

--- src/components/pages/mainPage/index.jsx
+++ src/components/pages/mainPage/index.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import MainCentral from "../../organism/main/mainCentral";
 import MainHeader from "../../organism/main/mainHeader"
 import MainFooter from "../../organism/main/mainFooter";
 
 const MainPage = () => {
+    useEffect(() => {
+        let topColor = document.getElementById("topColor")
+        topColor.content = "#ffffff"
+    })
     return (
         <>
             <MainHeader/>
